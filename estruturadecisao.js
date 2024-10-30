a5c8a2e4d05e5dd692dcd841fa588b62267fca07
Rodrigg197 <gr820300@gmail.com>
2024-10-30 19:32:16 -0300

    Add files via upload

---
+++ estruturadecisao.js
@@ -0,0 +1,23 @@
+let valor = 10
+
+if (valor >= 10) {
+    console.log("Você possui madeiras suficiente!")
+}else {
+    ("Você não possui madeiras suficiente!")
+}
+//............       
+ let  numero = "A"
+   
+ if ( numero < 0 ) {
+    console.log ("valor negativo")
+ }else if ( numero == 0 ) {
+    console.log("O valor é igual a zero")  
+ }else if ( numero >= 0 ) {
+    console.log("Valor positivo")
+ }else {
+    console.log("Valor desconhecido")
+ }
+
+ 
+    
+
